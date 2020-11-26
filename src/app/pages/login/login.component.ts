e75c8367b44a3b5030491d7eafb7d20f0e00c9f4
SaintJonii <Aguilera__j@hotmail.com>
2020-11-26 09:03:07 -0300

    login con toastr

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
+import { FormControl, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 
 @Component({
@@ -9,6 +11,15 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class LoginComponent implements OnInit {
 
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [Validators.required]);
+
+  regMailControl = new FormControl('', [Validators.required, Validators.email]);
+  regPassControl = new FormControl('', [Validators.required]);
+  regConfControl = new FormControl('', [Validators.required]);
+  nameControl = new FormControl('', [Validators.required]);
+
+
   log_email: string;
   log_pass: string;
 
@@ -17,7 +28,7 @@ export class LoginComponent implements OnInit {
   reg_confirm_pass: string;
   userName: string;
 
-  constructor(private auth: AuthService) { }
+  constructor(private auth: AuthService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
@@ -25,6 +36,7 @@ export class LoginComponent implements OnInit {
   login() {
     debugger;
     if (this.log_email == null || this.log_pass == null) {
+      this.toastr.warning("Debe Ingresar Email y Contraseña", "Campos Necesarios!");
       return;
     }
     else {
@@ -35,12 +47,37 @@ export class LoginComponent implements OnInit {
 
 
   register() {
-    if (this.reg_email == null || this.reg_pass == null || this.userName == null) {
+    if (this.reg_email == null || this.reg_pass == null || this.reg_confirm_pass == null || this.userName == null) {
+      this.toastr.warning("Debe Ingresar los campos requeridos", "Campos Necesarios");
+      return;
+    }
+    if (this.reg_pass != this.reg_confirm_pass) {
+      this.toastr.error("Debe repetir la misma contraseña", "Contraseñas distintas");
       return;
     }
     else {
       this.auth.registerUser(this.reg_email, this.reg_pass, this.userName);
     }
+  }
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'Ingresa mail';
+    }
+
+    if (this.password.hasError('required')) {
+      return 'Ingresa la contraseña';
+    }
+
+    if (this.regPassControl.hasError('required')) {
+      return 'Ingresa la contraseña';
+    }
+
+    if (this.regMailControl.hasError('email')) {
+      return 'No es un mail válido';
+    }
+
+    return this.email.hasError('email') ? 'No es un mail válido' : '';
   }
 
 }
